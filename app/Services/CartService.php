59877ab731147590b1dbe87c2db0b92e4dea09de
tienpk200101 <phungkhactien2001@gmail.com>
2023-03-02 16:12:47 +0700

    fix cart screen

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -5,6 +5,10 @@ namespace App\Services;
 class CartService
 {
     public function showCart() {
-        return view('admins.cart');
+        $title_page = 'Cart';
+
+        return view('admins.cart', [
+            'title_head' => $title_page
+        ]);
     }
 }
